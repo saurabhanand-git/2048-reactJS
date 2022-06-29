029b135c4f0b4f90be03b7102d0b58a194d9fb5a
saurabhanand-git <saurabhk3001@gmail.com>
2022-06-29 08:46:59 +0530

    hosting

---
+++ build/precache-manifest.d2f8aa9ae5536e4f43b4002b7d337b1d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a15ba262fdb09089290c00a3fda10570",
+    "url": "/2048-reactJS/index.html"
+  },
+  {
+    "revision": "7be3c4e3515a73cded4c",
+    "url": "/2048-reactJS/static/css/main.1469b127.chunk.css"
+  },
+  {
+    "revision": "430aa853b671a47b06f2",
+    "url": "/2048-reactJS/static/js/2.860ca6c0.chunk.js"
+  },
+  {
+    "revision": "82e8035e49740f791fa94175bc04ac4e",
+    "url": "/2048-reactJS/static/js/2.860ca6c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7be3c4e3515a73cded4c",
+    "url": "/2048-reactJS/static/js/main.6a3fc371.chunk.js"
+  },
+  {
+    "revision": "2ddf64ccc81d16adae88",
+    "url": "/2048-reactJS/static/js/runtime-main.6abf9090.js"
+  },
+  {
+    "revision": "e32dde8607904b31ca633a6f101715be",
+    "url": "/2048-reactJS/static/media/grid.e32dde86.svg"
+  },
+  {
+    "revision": "509ae2e4853e375295e29a09bb916116",
+    "url": "/2048-reactJS/static/media/restart.509ae2e4.svg"
+  },
+  {
+    "revision": "e263bfe98e5bbf96976110700709f636",
+    "url": "/2048-reactJS/static/media/undo.e263bfe9.svg"
+  }
+]);
